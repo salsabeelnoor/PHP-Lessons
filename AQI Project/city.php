bad7ab59c986b29ece741c6e2c4dc684ad37e475
Salsabeel Noor <salsabeelnoor@gmail.com>
2025-09-09 13:59:39 +0600

    finished

--- AQI Project/city.php
+++ AQI Project/city.php
@@ -7,15 +7,15 @@ if(!empty($_GET['city'])) {
     $city = $_GET['city'];
 }
 $filename = null;
+$cityInformation = [];
 if(!empty($city)) {
-    //load data
-    $cities = json_decode(file_get_contents(__DIR__ . '/data/index.json'), true);
-    //prepare data
 
+    $cities = json_decode(file_get_contents(__DIR__ . '/data/index.json'), true);
 
     foreach($cities AS $c) {
         if($c['city'] === $city) {
             $filename = $c['filename'];
+            $cityInformation = $c;
             break;
         }
     }
@@ -23,6 +23,21 @@ if(!empty($city)) {
 $stats = [];
 if(!empty($filename)) {
     $results = json_decode(file_get_contents('compress.bzip2://' . __DIR__ . '/data/' . $filename), true)['results'];
+
+    $units = [
+        'pm25' => null,
+        'pm10' => null
+    ];
+
+    foreach ($results as $result) {
+        if(!empty($units['pm25'] && !empty($units['pm10']))) break;
+        if($result['parameter'] === 'pm25') {
+            $units['pm25'] = $result['unit'];
+        }
+        if($result['parameter'] === 'pm10') {
+            $units['pm10'] = $result['unit'];
+        }
+    }
     
     foreach($results AS $result) {
         if(($result['parameter'] !== 'pm25' && $result['parameter'] !== 'pm10') || $result['value'] < 0) continue;
@@ -45,8 +60,57 @@ if(!empty($filename)) {
 
 <?php if(empty($city)) : ?>
     <p>City Could not be loaded!</p>
-<?php else: 
+<?php else: ?> 
+    <h1><?php echo e($cityInformation['city']); ?></h1>
+    <?php
     if(!empty($stats)): ?>
+    <canvas id="aqi-chart" style="width: 300px; height: 300px;"></canvas>
+    <script src="./scripts/chart.umd.js"></script>
+    <?php 
+        $labels = array_keys($stats);
+        sort($labels);
+        $pm25 = [];
+        $pm10 = [];
+        foreach ($labels as $label) {
+            $measurements = $stats[$label];
+            $pm25[] = round(array_sum($measurements['pm25'])/count($measurements['pm25']), 5);
+            $pm10[] = round(array_sum($measurements['pm10'])/count($measurements['pm10']), 5);
+        }
+    ?>
+    <script>
+        const ctx = document.getElementById('aqi-chart');
+        const chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: <?php echo json_encode($labels); ?>,
+                datasets: [
+                {
+                    label: <?php echo json_encode("AQI, PM2.5 in {$units['pm25']}"); ?>,
+                    data: <?php echo json_encode($pm25); ?>,
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                },
+                {
+                    label: <?php echo json_encode("AQI, PM10 in {$units['pm10']}"); ?>,
+                    data: <?php echo json_encode($pm10); ?>,
+                    fill: false,
+                    borderColor: 'rgb(192, 75, 192)',
+                    tension: 0.1
+                }
+            ]
+            },
+            options: {
+                onClick: (e) => {
+                const canvasPosition = getRelativePosition(e, chart);
+
+                // Substitute the appropriate scale IDs
+                const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+                const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+                }
+            }
+        });
+    </script>
     <table>
         <thead>
             <tr>
@@ -59,8 +123,8 @@ if(!empty($filename)) {
             <?php foreach($stats AS $month => $measurements): ?>
                 <tr>
                     <th><?php echo e($month); ?></th>
-                    <td><?php echo e(array_sum($measurements['pm25'])/count($measurements['pm25'])); ?></td>
-                    <td><?php echo e(array_sum($measurements['pm10'])/count($measurements['pm10'])); ?></td>
+                    <td><?php echo e(round(array_sum($measurements['pm25'])/count($measurements['pm25']), 2)); echo e($units['pm25']); ?> </td>
+                    <td><?php echo e(round(array_sum($measurements['pm10'])/count($measurements['pm10']), 2)); echo e($units['pm25']);?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
